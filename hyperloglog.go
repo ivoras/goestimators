9b4e969c8e78a6837c2d5b5bb22a7613e265c7b9
Ivan Voras <ivoras@gmail.com>
2017-02-02 20:12:27 +0100

    Introduce variable alpha factor

--- hyperloglog.go
+++ hyperloglog.go
@@ -17,6 +17,7 @@ func (a byteSlice) Less(i, j int) bool { return a[i] < a[j] }
 // LogLog is the structure with book-keeping data for the LogLog calculations.
 type LogLog struct {
 	buckets []byte
+	alpha   float64
 	bucmask uint
 	bucbits byte
 }
@@ -30,6 +31,17 @@ func NewLogLog(nBuckets uint) (*LogLog, error) {
 	ll.buckets = make([]byte, nBuckets)
 	ll.bucmask = nBuckets - 1
 	ll.bucbits = countTrailing1InUint64Alt(uint64(ll.bucmask))
+
+	switch nBuckets {
+	case 16:
+		ll.alpha = 0.673
+	case 32:
+		ll.alpha = 0.697
+	case 64:
+		ll.alpha = 0.709
+	default:
+		ll.alpha = 0.7213 / (1.0 + 1.079/float64(nBuckets))
+	}
 	return &ll, nil
 }
 
@@ -51,7 +63,7 @@ func (l *LogLog) Estimate() uint64 {
 		sum += uint(b)
 	}
 	average := float64(sum) / float64(len(l.buckets))
-	return uint64(math.Pow(2, average) * float64(len(l.buckets)) * 0.79402)
+	return uint64(math.Pow(2, average) * float64(len(l.buckets)) * l.alpha)
 }
 
 // SuperEstimate returns a cardinality estimation based on the SuperLogLog modification of the LogLog algorithm
@@ -71,15 +83,9 @@ func (l *LogLog) SuperEstimate() uint64 {
 	return uint64(math.Pow(2, average) * float64(cutoff) * 0.79402)
 }
 
-// HyperLogLog is the structure with book-keeping data for the HyperLogLog calculations.
-type HyperLogLog struct {
-	x int
-}
-
-// Observe makes the LogLog calculation for the given byte array, "counting" it
-// in its data structure.
-func (hl *HyperLogLog) Observe(b []byte) {
-
+// HyperEstimate returns a cardinality estimation based on the HyperLogLog modification of the SuperLogLog algorithm
+func (l *LogLog) HyperEstimate() uint64 {
+	return 0
 }
 
 // ----------------------------------------------------
